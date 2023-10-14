3566d0171d5a9e2da1cf3330ee8ca6493154f144
Hunter Gaylord <huntergaylord209@gmail.com>
2023-10-14 10:56:45 -0600

    done

--- frontend/components/Card.js
+++ frontend/components/Card.js
@@ -1,14 +1,37 @@
 import React from "react";
 import Figure from "./Figure";
+import styled from "styled-components";
+
+const StyledCard = styled.div `
+border : 3px solid black;
+padding : 5rem;
+border-radius : 20px;
+
+h2 {
+    color : ${pr => pr.$color};
+    margin : 0 0 1rem 0;
+    font-size : 2.5em;
+}
+
+p {
+    margin : 0 0 2rem 0;
+    padding : 1rem;
+
+    &::first-line{
+        font-size : 1.5em;
+    }
+}
+`
+
 export default function Card({title, text, imageurl, date}) {
     return(
-       <div className='card'>
+       <StyledCard $color= 'orange'className='card'>
           <h2>{title}</h2>
           <p>{text}</p>
          <Figure
          imageurl={imageurl}
          caption={date}
          />
-        </div>
+        </StyledCard>
     )
 }
--- frontend/components/Figure.js
+++ frontend/components/Figure.js
@@ -1,8 +1,19 @@
 import React from "react"
+import styled from "styled-components"
+
+const StyledFigure = styled.figure`
+max-width : 55%
+
+figcaption {
+  font-style : italic;
+  
+}
+`
+
 export default function Figure({imageurl, caption}){
-    return(<figure>
+    return(<StyledFigure>
            <img src={imageurl}/>
            <figcaption>Amazing Picture taken on {caption}</figcaption>
-         </figure>
+         </StyledFigure>
     )
  }
